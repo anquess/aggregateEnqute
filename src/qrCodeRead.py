51349eae417317ed0bc102d26ea307a88a3c7781
higashiyama-k105r <higashiyama-k105r@tsuushin1.onmicrosoft.com>
2020-08-25 15:03:59 +0900

    [feat]qrCodeRead.pyをmain関数化、合わせて引数のエラー処理を追加

--- src/qrCodeRead.py
+++ src/qrCodeRead.py
@@ -1,3 +1,5 @@
+import sys
+import os
 def qrCodeToStr(filePath):
     """QRコードから文字列を読み取る
 
@@ -13,4 +15,15 @@ def qrCodeToStr(filePath):
     # QRコードデコード
     qr = cv2.QRCodeDetector()
     data,_,_ = qr.detectAndDecode(img)
+    print(data)
     return data
+
+if __name__ == '__main__':
+    args = sys.argv
+    if 2 == len(args):
+        if os.path.isfile(args[1]):
+            qrCodeToStr(args[1])
+        else:
+            print(args[1] + 'というファイルはありません')
+    else:
+        print(args + '引数は1つです')
