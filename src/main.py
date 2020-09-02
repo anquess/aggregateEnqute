7a9dddef55f9fa62a42eec6ddd163cfe6c9eaf31
higashiyama-k105r <higashiyama-k105r@tsushin1.onmicrosoft.com>
2020-09-02 14:46:57 +0900

    [futute]csv保存の形式を変更　アンケート集計しやすくするため

--- src/main.py
+++ src/main.py
@@ -7,23 +7,20 @@ def main():
     resultJpgFiles = glob.glob('img\\enquete\\*.jpg')
     
     for resultJpgFile in resultJpgFiles:
-        csvFileName = qrCodeRead.qrCodeToStr(resultJpgFile)
-        if not csvFileName == "":
-#            outputCsvFile = open('result/' + csvFileName + '.csv',mode='w',newline="")
-#            with open('result/' + csvFileName + '.csv',mode='w',newline="")as f:
-            with open('result/result.csv',mode='a',newline="")as f:
-            
+        qrMessage = qrCodeRead.qrCodeToStr(resultJpgFile)
+        if not qrMessage == "":
+            enqueteId = (qrMessage.split('_')[0] + '_' + qrMessage.split('_')[1])
+            personalId = qrMessage.split('_')[2:4]
+            n_col = int(qrMessage.split('_')[4])
+            n_row = int(qrMessage.split('_')[5])
+
+            with open('result/' + enqueteId + '.csv',mode='a',newline="")as f:
+
                 writer = csv.writer(f)
-
-                message = (csvFileName.split('_')[0:4])
-                n_col = int(csvFileName.split('_')[4])
-                n_row = int(csvFileName.split('_')[5])
-
-                resultLine = changeMark.changeMarkToStr(resultJpgFile,n_col,n_row,message)
+                resultLine = changeMark.changeMarkToStr(resultJpgFile,n_col,n_row,personalId)
                 if not resultLine == 'error':
                     print(resultLine)
                     writer.writerow(resultLine)
-#                    outputCsvFile.close()
                 else:
                     with open('result/err.txt',mode='a',newline="") as errTxt:
                         errTxt.writerows('err:マークシートが読めなかった,    FileName=' + resultJpgFile)
